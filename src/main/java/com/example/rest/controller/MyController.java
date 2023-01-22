addf2a04971ab0058f9fa40c6996ba668a803474
mallika <tirumala.mreddy@gmail.com>
2023-01-22 15:41:00 +0530

    hhh

--- src/main/java/com/example/rest/controller/MyController.java
+++ src/main/java/com/example/rest/controller/MyController.java
@@ -13,7 +13,6 @@ public class MyController {
 		System.out.println("github33");
 		System.out.println("github111");
 		System.out.println("hhhhhhh");
-		System.out.println("github444888111");
 		return "say hello world!!!";
 	}
 	@GetMapping("/hello1")
