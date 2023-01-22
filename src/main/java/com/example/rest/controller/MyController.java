20750d2e46ca268371c1f04cd5e60dc1551a5d95
DeveloperGitBash <55375940+DeveloperGitBash@users.noreply.github.com>
2023-01-22 15:09:25 +0530

    fff

--- src/main/java/com/example/rest/controller/MyController.java
+++ src/main/java/com/example/rest/controller/MyController.java
@@ -15,5 +15,9 @@ public class MyController {
 		System.out.println("github111");
 		return "say hello world!!!";
 	}
-	
+	@GetMapping("/hello1")
+	public String getHello1() {
+		System.out.println("github111");
+		return "say hello world!!!";
+	}
 }
