492d264910f7c3a7ffbcce367a633d54cfa7c1c7
DeveloperGitBash <55375940+DeveloperGitBash@users.noreply.github.com>
2023-01-22 14:34:06 +0530

    remove

--- src/main/java/com/example/rest/controller/MyController.java
+++ src/main/java/com/example/rest/controller/MyController.java
@@ -11,7 +11,6 @@ public class MyController {
 	@GetMapping("/hello")
 	public String getHello() {
 		System.out.println("eclipse");
-		System.out.println("github");
 		return "say hello world!!!";
 	}
 }
