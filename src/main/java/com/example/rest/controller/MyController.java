86c49de71de860047af92a5615ad19390b2d2fb7
mallika <tirumala.mreddy@gmail.com>
2023-01-22 15:29:10 +0530

    ffff

--- src/main/java/com/example/rest/controller/MyController.java
+++ src/main/java/com/example/rest/controller/MyController.java
@@ -12,7 +12,7 @@ public class MyController {
 	public String getHello() {
 		System.out.println("github33");
 		System.out.println("github111");
-		System.out.println("github444888111222");
+		System.out.println("hhhhhhh");
 		System.out.println("github444888111");
 		return "say hello world!!!";
 	}
