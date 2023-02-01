d444dc1c69eb9ed2c0d25bdb3d24c1536350fe37
DeveloperGitBash <55375940+DeveloperGitBash@users.noreply.github.com>
2023-02-01 19:08:22 +0530

    sss

--- src/main/java/com/example/rest/controller/MyController.java
+++ src/main/java/com/example/rest/controller/MyController.java
@@ -10,7 +10,7 @@ public class MyController {
 
 	@GetMapping("/hello")
 	public String getHello() {
-		System.out.println("github331113333");
+		System.out.println("github3311133331");
 		System.out.println("github111");
 		System.out.println("hhhhhhh11166");
 		System.out.println("hhhhhhh1114445");
@@ -18,7 +18,15 @@ public class MyController {
 	}
 	@GetMapping("/hello1")
 	public String getHello2() {
-		System.out.println("github331113333");
+		System.out.println("github3311133313");
+		System.out.println("github111");
+		System.out.println("hhhhhhh11166");
+		System.out.println("hhhhhhh111444");
+		return "say hello world1112";
+	}
+	@GetMapping("/hello3")
+	public String getHello3() {
+		System.out.println("github3311131333");
 		System.out.println("github111");
 		System.out.println("hhhhhhh11166");
 		System.out.println("hhhhhhh111444");
